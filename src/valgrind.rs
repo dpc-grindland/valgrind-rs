b61b53e7ddb0d8fd8ce62b43f785e407903df160
Daniel Trebbien <dtrebbien@gmail.com>
2014-07-16 07:58:26 -0400

    Remove the `crate_id' attribute

--- src/valgrind.rs
+++ src/valgrind.rs
@@ -10,7 +10,6 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // Lesser General Public License for more details.
 
-#![crate_id = "valgrind#0.1.0"]
 #![crate_type = "lib"]
 #![feature(struct_variant)]
 
